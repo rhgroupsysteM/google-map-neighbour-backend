b86194839904261c803463c3005b2b3f19c9c443
Leandro Miguel Narciso <lmnnarciso14@gmail.com>
2020-06-06 00:58:49 +0800

    allow cors

--- server.js
+++ server.js
@@ -13,7 +13,31 @@ const fsPromises = fs.promises;
 // eslint-disable-next-line no-undef
 const port = process.env.PORT || 7000;
 app.use(express.json());
-app.use(cors());
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
+// app.use(cors());
 
 async function walk(dir) {
   let files = await fs.readdir(dir);
